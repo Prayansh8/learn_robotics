e9643d6ccc498ef8898122e3c571a9d385e7abf6
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2023-06-12 13:03:39 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 // import Grid from "./components/Grid";
 import GridBoxes from "./components/Dev";
-
+import "./App.css"
 const App = () => {
   return (
-    <div>
-      <h1>Grid of Boxes</h1>
+    <div className="App">
       {/* <Grid /> */}
       <GridBoxes />
     </div>
