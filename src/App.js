dd07216cdda53a70b7ef8d02ce9f124e0be52107
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2023-09-02 18:06:39 +0530

    pusgh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
 import Grid from "./components/Grid";
+import Footer from "./components/Footer";
+
 const App = () => {
   return (
     <div className="App">
       <Grid />
+      <Footer />
     </div>
   );
 };
