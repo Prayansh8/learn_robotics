0562422bcea6cd2d82f55bd06f05fd715c1e8e41
Prayansh <91784825+Prayansh8@users.noreply.github.com>
2023-11-13 23:55:55 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,17 @@ import React from "react";
 import "./Footer.css"; // Import the CSS file
 
 const Footer = () => {
-    
   return (
     <div class="footer">
-      &copy; 2023 Robotics Education Platform - Developed by{" "}
-      <span class="red-text">Prayansh Gupta.</span>
+      <a href="/" className="mb-md-0 color-white text-decoration-none lh-1">
+        <img src={logo} alt="Company logo" className="logoimg color-white" />
+      </a>
+      &copy; 2023 Robotics Education Platform - Developed by&nbsp;
+      <span>
+        <a href="https://prayanshgupta.com/" class="red-text">
+          Prayansh Gupta.
+        </a>
+      </span>
     </div>
   );
 };
